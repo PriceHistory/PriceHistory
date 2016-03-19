ebad849535499b5b3f6b5ce1720dc9232f8f44f3
vgotsuliak <liddder@gmail.com>
2016-03-19 08:45:14 +0200

    add call migrate.sh after waiting 10 seconds because we need to wait while postgresql is starting up

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -6,4 +6,6 @@ docker rm -fv ph-db
 docker build -f docker/Dockerfile -t price-history . && \
 
 docker run --name ph-db -e POSTGRES_PASSWORD=pass1234 -e POSTGRES_DB=postgres -e POSTGRES_USER=postgres -d postgres
-docker run --name price-history -p 8080:8080 -it -v $(pwd):/go/src/app --link ph-db:postgres price-history /bin/bash
+sleep 10s #need to wait while postgresql starting up
+docker run --name price-history -p 8080:8080 -it -v $(pwd):/go/src/app --link ph-db:postgres price-history bash -c "./scripts/migrate.sh && /bin/bash"
+
